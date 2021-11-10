cec6a586a2f15102978863350c2783ba2d36be37
quannnl92@gmail.com <quannnl92@gmail.com>
2021-11-10 12:43:06 +0000

    Get from spreadsheet and transform

--- models/test/test.sql
+++ models/test/test.sql
@@ -1,7 +1,18 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-select
-    "test" AS test_id,
-    "test" AS test_1,
-    "test" AS test_2,
-    "test" AS test_3
+WITH partition_for_deduplicate AS (
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY ma_don_hang, ma_san_pham ORDER BY Ngay_Xuat DESC) AS index_by_ma_ncc,
+        *
+    FROM
+        {{ ref('raw_data_nivea_tiki') }}
+)
+SELECT
+    *
+    EXCEPT(
+        index_by_ma_ncc
+    )
+FROM
+    partition_for_deduplicate
+WHERE
+    index_by_ma_ncc = 1
