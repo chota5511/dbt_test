f8f97710255d837a219573e963bcd7ff97e3b565
quannnl92@gmail.com <quannnl92@gmail.com>
2021-11-12 04:17:44 +0000

    test 2

--- models/test/test.sql
+++ models/test/test.sql
@@ -1,5 +1,3 @@
-{{ config(materialized='view') }}
-
 WITH partition_for_deduplicate AS (
     SELECT
         ROW_NUMBER() OVER (PARTITION BY ma_don_hang, ma_san_pham ORDER BY Ngay_Xuat DESC) AS index_by_ma_ncc,
